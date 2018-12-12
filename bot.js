a0b9d5a5d900ac39be852f7022a75f1db302f322
MrAshrafa <45470917+MrAshrafa@users.noreply.github.com>
2018-12-12 18:04:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,25 +11,6 @@ client.on('message', msg => {
   }
 });
 
-client.on('message', function(msg) {
-    if(msg.content.startsWith (prefix + 'server')) {
-      if(!msg.channel.guild) return msg.reply('**:x: اسف لكن هذا الامر للسيرفرات فقط **');         
-      let embed = new Discord.RichEmbed()
-      .setColor('RANDOM')
-      .setThumbnail(msg.guild.iconURL)
-      .addField(':globe_with_meridians: **اسم السيرفر : **' , `**[ ${msg.guild.name} ]**`,true)
-      .addField(':earth_africa: ** موقع السيرفر :**',`**[ ${msg.guild.region} ]**`,true)
-      .addField(':military_medal:** الرتب :**',`**[ ${msg.guild.roles.size} ]**`,true)
-      .addField(':bust_in_silhouette:** عدد الاعضاء :**',`**[ ${msg.guild.memberCount} ]**`,true)
-      .addField(':white_check_mark:** عدد الاعضاء الاونلاين :**',`**[ ${msg.guild.members.filter(m=>m.presence.status == 'online').size} ]**`,true)
-      .addField(':pencil:** الرومات الكتابية :**',`**[ ${msg.guild.channels.filter(m => m.type === 'text').size} ]**`,true)
-      .addField(':loud_sound:** رومات الصوت :**',`**[ ${msg.guild.channels.filter(m => m.type === 'voice').size} ]**`,true)
-      .addField(':crown:** صاحب السيرفر :**',`**[ ${msg.guild.owner} ]**`,true)
-      .addField(':id:** ايدي السيرفر :**',`**[ ${msg.guild.id} ]**`,true)
-      .addField(':date:** تم عمل السيرفر في : **',msg.guild.createdAt.toLocaleString())
-      msg.channel.send({embed:embed});
-    }
-  });
 
 client.on('message', message => {
               if(!message.channel.guild) return;
@@ -37,7 +18,7 @@ client.on('message', message => {
     if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
   if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );  //7md
     let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-    let copy = "GiantBot👑";
+    let copy = "WolfBot";
     let request = `Requested By ${message.author.username}`;  //7md
     if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
     msg.react('✅')
